fd83a98278cc63b2471c05e87759d387c10a7c3d
Fule <laki.linux@gmail.com>
2021-07-23 00:14:54 -0700

    create connecton to database

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require ('express');
-
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3002;
 const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // mysql user connection credentials
+        user: 'root', password: 'root',
+        database: 'my_employee'
+    },
+    console.log('Connected to the my Employee database.')
+);
 
 // test connection
 app.get('/', (req, res) => {
